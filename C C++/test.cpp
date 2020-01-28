d67edab4bc9cc05884f1cf724b9e5bb3deba6796
Dan <dan.k2095@gmail.com>
2020-01-27 21:37:53 -0500

    1/27/2020

---
+++ C C++/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+  int numberOfBitches;
+  cout << "Enter the number of bitches\n";
+  cin >> numberOfBitches;
+  for(int i = 0;i==10;i++)
+  {
+      numberOfBitches += 1;
+  }
+  cout << numberOfBitches;
+  return 1;
+};
